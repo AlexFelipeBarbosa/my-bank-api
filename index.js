639dc8f9dd7b27b5342928ff46c4b83688f7f6dc
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-06-03 10:41:44 -0300

    Aula 9.1 - Metodo POST - Parte 1

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello Word');
+});
+
+// Metodo POST
+app.post('/account', (req, res) => {
+  console.log('post');
 });
 
 app.listen(3000, function () {
