fa0df9bc7f5bb9f9a2baf47c7cb3d35c24abbe86
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-06-03 11:57:59 -0300

    Aula 9.3 - Metodo POST - Parte 3

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-var express = require('express');
-var fs = require('fs'); // file system para trabalhar com arquivos
+let express = require('express');
+let fs = require('fs'); // file system para trabalhar com arquivos
 
-var app = express();
+let app = express();
 app.use(express.json()); // utilizando objetos JSON
-
-app.get('/', function (req, res) {
-  res.send('Hello Word');
-});
 
 // Metodo POST
 app.post('/account', (req, res) => {
   let params = req.body; // recebemento os parametros passados no POST
 
+  // Primeiro passo fazer a leitura dos registros existentes
+  fs.readFile('./dados/accounts.json', 'utf-8', (err, data) => {
+    console.log(err);
+
+    try {
+      // Convertendo String para JSON
+      let json = JSON.parse(data);
+      res.send('post');
+      console.log(json);
+    } catch (error) {
+      res.send();
+    }
+  });
+
   //gravando o arquivo
   // JSON.stringify(params) - convertendo o params que é um JSON para String
   // fs.writeFile - substitui o registro existente pelo novo
   // fs.appendFile - inclui um novo registro, mantendo o existente
+
+  /*
   fs.appendFile('./dados/accounts.json', JSON.stringify(params), (err) => {
     console.log(err);
   });
-
-  res.send('post');
+*/
 });
 
 app.listen(3000, function () {
