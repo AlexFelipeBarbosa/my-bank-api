ab1bf8d36c98efc2665eb1750c22fecb69e0e840
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-06-03 15:29:07 -0300

    Aula 10.2 - Router

--- index.js
+++ index.js
@@ -1,71 +1,18 @@
 let express = require('express');
 let fs = require('fs'); // file system para trabalhar com arquivos
-const dataLink = './dados/account.json'; // caminho do Json (registros)
+global.fileName = './dados/account.json'; // caminho do Json (registros)
 
 let app = express();
+let accountsRouter = require('./routes/accounts.js');
+
 app.use(express.json()); // utilizando objetos JSON
-
-// Metodo POST
-app.post('/account', (req, res) => {
-  let account = req.body; // recebemento os parametros passados no POST
-  // Primeiro passo fazer a leitura dos registros existentes
-  fs.readFile(dataLink, 'utf-8', (err, data) => {
-    if (!err) {
-      try {
-        // Convertendo String para JSON
-        let json = JSON.parse(data);
-        account = { id: json.nextId, ...account }; // ... pega toda a estrutura do account
-        json.nextId++; // incrementando o ID
-        json.accounts.push(account); // inserindo o novo account
-
-        //Escrevendo no account
-        fs.writeFile(dataLink, JSON.stringify(json), (err) => {
-          if (err) {
-            res.status(400).send({ error: err.message });
-          } else {
-            res.end();
-          }
-        });
-      } catch (error) {
-        res.status(400).send({ error: err.message });
-      }
-    } else {
-      res.status(400).send({ error: err.message });
-    }
-  });
-
-  //gravando o arquivo
-  // JSON.stringify(params) - convertendo o params que é um JSON para String
-  // fs.writeFile - substitui o registro existente pelo novo
-  // fs.appendFile - inclui um novo registro, mantendo o existente
-
-  /*
-  fs.appendFile(dataLink, JSON.stringify(params), (err) => {
-    console.log(err);
-  });
-*/
-});
-
-// Metodo GET
-// Retornando todos os registros.
-app.get('/account', (_, res) => {
-  // ler o arquivo accounts.json
-  fs.readFile(dataLink, 'utf8', (err, data) => {
-    if (!err) {
-      let dataJson = JSON.parse(data);
-      delete dataJson.nextId; // retirando o nextId, ou seja o nextId não será retornado no GET
-      res.send(dataJson);
-    } else {
-      res.status(400).send({ error: err.message });
-    }
-  });
-});
+app.use('/account', accountsRouter);
 
 // Iniciando a API na porta 3000
 app.listen(3000, function () {
   // Ao iniciar a Aplicação, vamos verificar se o arquivo de registro existe
   try {
-    fs.readFile(dataLink, 'utf8', (err, data) => {
+    fs.readFile(global.fileName, 'utf8', (err, data) => {
       // Se der erro é porque ele não existe ainda.
       if (err) {
         //Caso não exista, será criado agora
@@ -73,7 +20,7 @@ app.listen(3000, function () {
           nextId: 1,
           accounts: [],
         };
-        fs.writeFile(dataLink, JSON.stringify(initialJson), (err) => {
+        fs.writeFile(global.fileName, JSON.stringify(initialJson), (err) => {
           if (err) {
             console.log(err);
           }
---
+++ routes/accounts.js
@@ -0,0 +1,61 @@
+let express = require('express');
+let fs = require('fs'); // file system para trabalhar com arquivos
+var router = express.Router();
+
+// Metodo POST
+router.post('/', (req, res) => {
+  let account = req.body; // recebemento os parametros passados no POST
+  // Primeiro passo fazer a leitura dos registros existentes
+  fs.readFile(global.fileName, 'utf-8', (err, data) => {
+    if (!err) {
+      try {
+        // Convertendo String para JSON
+        let json = JSON.parse(data);
+        account = { id: json.nextId, ...account }; // ... pega toda a estrutura do account
+        json.nextId++; // incrementando o ID
+        json.accounts.push(account); // inserindo o novo account
+
+        //Escrevendo no account
+        fs.writeFile(global.fileName, JSON.stringify(json), (err) => {
+          if (err) {
+            res.status(400).send({ error: err.message });
+          } else {
+            res.end();
+          }
+        });
+      } catch (error) {
+        res.status(400).send({ error: err.message });
+      }
+    } else {
+      res.status(400).send({ error: err.message });
+    }
+  });
+
+  //gravando o arquivo
+  // JSON.stringify(params) - convertendo o params que é um JSON para String
+  // fs.writeFile - substitui o registro existente pelo novo
+  // fs.appendFile - inclui um novo registro, mantendo o existente
+
+  /*
+  fs.appendFile(global.fileName, JSON.stringify(params), (err) => {
+    console.log(err);
+  });
+*/
+});
+
+// Metodo GET
+// Retornando todos os registros.
+router.get('/', (_, res) => {
+  // ler o arquivo accounts.json
+  fs.readFile(global.fileName, 'utf8', (err, data) => {
+    if (!err) {
+      let dataJson = JSON.parse(data);
+      delete dataJson.nextId; // retirando o nextId, ou seja o nextId não será retornado no GET
+      res.send(dataJson);
+    } else {
+      res.status(400).send({ error: err.message });
+    }
+  });
+});
+
+module.exports = router;
